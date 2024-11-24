ac113b45a0bb3bc5e2e6ba341814165ea79ec3c7
Napwob <gorno-altaisk0@mail.ru>
2024-11-24 21:51:11 +0300

    Manual Enter

--- src/main/java/org/example/menu/EnterData.java
+++ src/main/java/org/example/menu/EnterData.java
@@ -1,48 +1,157 @@
 package org.example.menu;
 
+import org.example.essence.Animal;
+import org.example.essence.Barrel;
+import org.example.essence.Human;
 import org.example.singletoneArray.SingletoneArray;
-import org.example.strategies.fill.FillFromFile;
-import org.example.util.validate.ScannerValidate;
 import org.example.util.validate.TypeValidation;
 
 public final class EnterData extends BaseVariant {
 
 	SingletoneArray array = SingletoneArray.getInstance();
-	
-    @Override
-    public void menu() {
-        System.out.println("Data input mode selected");
 
-        int validVariant = 0;
+	@Override
+	public void menu() {
+		System.out.println("Data input mode selected");
 
-        
-        
-        while (true) {
-            System.out.println("Please, select type of input:");
-            System.out.println(" 1 - Manual data entry");
-            System.out.println(" 2 - Random data");
-            System.out.println(" 3 - Read data from file");
-            System.out.println(" 4 - Exit");
+		int validVariant = 0;
 
-            System.out.print("Please, enter your chosen action: ");
-            validVariant = (int) scannerValidate.getValidValue(TypeValidation.VARIANTS_4);
-              
-            switch (validVariant) {
+		while (true) {
+			System.out.println("Please, select type of input:");
+			System.out.println(" 1 - Manual data entry");
+			System.out.println(" 2 - Random data");
+			System.out.println(" 3 - Read data from file");
+			System.out.println(" 4 - Exit");
+
+			System.out.print("Please, enter your chosen action: ");
+			validVariant = (int) scannerValidate.getValidValue(TypeValidation.VARIANTS_4);
+
+			switch (validVariant) {
 			case (1):
-				//enterData.menu();
+				manualEntry();
 				break;
 			case (2):
 				// Код2;
 				break;
 			case (3):
-				
+
 				break;
 			default:
 			}
-            if (validVariant == 4) {
-                System.out.println("Return to main menu");
-                break;
-            }
-        }
-    }
+			if (validVariant == 4) {
+				System.out.println("Return to main menu");
+				break;
+			}
+		}
+	}
+
+	private void manualEntry() {
+		int validVariant = 0;
+
+		if (array.isEmpty()) {
+			while (true) {
+				System.out.println("Please, select type of entity:");
+				System.out.println(" 1 - Animal");
+				System.out.println(" 2 - Barrel");
+				System.out.println(" 3 - Human");
+				System.out.println(" 4 - Exit");
+
+				System.out.print("Please, enter your chosen action: ");
+				validVariant = (int) scannerValidate.getValidValue(TypeValidation.VARIANTS_4);
+
+				switch (validVariant) {
+				case (1):
+					handleEntityEntry(this::createAnimalEntry);
+					break;
+				case (2):
+					handleEntityEntry(this::createBarrelEntry);
+					break;
+				case (3):
+					handleEntityEntry(this::createHumanEntry);
+					break;
+				default:
+				}
+				if (validVariant == 4) {
+					System.out.println("Returning to data input menu.");
+					break;
+				}
+			}
+		} else {
+			Object firstEntity = array.get(0);
+			if (firstEntity instanceof Animal) {
+				System.out.println("The array already contains Animals. You can add more Animals.");
+				handleEntityEntry(this::createAnimalEntry);
+			} else if (firstEntity instanceof Barrel) {
+				System.out.println("The array already contains Barrels. You can add more Barrels.");
+				handleEntityEntry(this::createBarrelEntry);
+			} else if (firstEntity instanceof Human) {
+				System.out.println("The array already contains Humans. You can add more Humans.");
+				handleEntityEntry(this::createHumanEntry);
+			}
+		}
+	}
+
+	private void handleEntityEntry(Runnable entityEntryMethod) {
+		while (true) {
+			entityEntryMethod.run();
+
+			System.out.print(
+					"Do you want to add another entity of the same type? (Type 'No' to stop or 'Yes' to continue): ");
+			String input = scannerValidate.getValidValue(TypeValidation.STRING).toString().trim().toLowerCase();
+
+			if (input.equals("No")) {
+				System.out.println("Exiting entity creation.");
+				break;
+			}
+		}
+	}
+
+	private void createAnimalEntry() {
+		System.out.print("Enter animal species: ");
+		String species = (String) scannerValidate.getValidValue(TypeValidation.STRING);
+
+		System.out.print("Enter animal eye color: ");
+		String eyeColor = (String) scannerValidate.getValidValue(TypeValidation.STRING);
+
+		System.out.print("Does the animal have fur? (true/false): ");
+		boolean hasFur = (boolean) scannerValidate.getValidValue(TypeValidation.BOOLEAN);
+
+		Animal animal = new Animal.AnimalBuilder().species(species).eyeColor(eyeColor).hasFur(hasFur).build();
+
+		array.add(animal);
+		System.out.println("Animal created: " + animal);
+	}
+
+	private void createBarrelEntry() {
+		System.out.print("Enter barrel volume: ");
+		double volume = (float) scannerValidate.getValidValue(TypeValidation.FLOAT);
+
+		System.out.print("Enter stored material: ");
+		String storedMaterial = (String) scannerValidate.getValidValue(TypeValidation.STRING);
+
+		System.out.print("Enter material the barrel is made of: ");
+		String materialMadeOf = (String) scannerValidate.getValidValue(TypeValidation.STRING);
+
+		Barrel barrel = new Barrel.BarrelBuilder().volume(volume).storedMaterial(storedMaterial)
+				.materialMadeOf(materialMadeOf).build();
+
+		array.add(barrel);
+		System.out.println("Barrel created: " + barrel);
+	}
+
+	private void createHumanEntry() {
+		System.out.print("Enter human gender: ");
+		String gender = (String) scannerValidate.getValidValue(TypeValidation.STRING);
+
+		System.out.print("Enter human age: ");
+		int age = (int) scannerValidate.getValidValue(TypeValidation.INT);
+
+		System.out.print("Enter human surname: ");
+		String surname = (String) scannerValidate.getValidValue(TypeValidation.STRING);
+
+		Human human = new Human.HumanBuilder().gender(gender).age(age).surname(surname).build();
+
+		array.add(human);
+		System.out.println("Human created: " + human);
+	}
 }
--- src/main/java/org/example/menu/Menu.java
+++ src/main/java/org/example/menu/Menu.java
@@ -1,9 +1,75 @@
 package org.example.menu;
 
+import org.example.singletoneArray.SingletoneArray;
+import org.example.util.validate.ScannerValidate;
+import org.example.util.validate.TypeValidation;
+
 public final class Menu {
 
-	public void execute() {
-		
+	ScannerValidate scannerValidate = new ScannerValidate();
+
+	private static void clearConsole() {
+		try {
+			String os = System.getProperty("os.name").toLowerCase();
+			if (os.contains("win")) {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} else {
+				new ProcessBuilder("clear").inheritIO().start().waitFor();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void execute() {
+		System.out.println("Welcome to SimpleDataBase");
+
+		int validVariant = 0;
+		
+		EnterData enterData = new EnterData();
+		SortData sortData = new SortData();
+		SearchData searchData = new SearchData();
+
+		while (true) {
+			// clearConsole(); TODO doesn't work in eclipse
+			System.out.println("Please, select one of three variants:");
+			System.out.println(" 1 - Enter");
+			System.out.println(" 2 - Sort");
+			System.out.println(" 3 - Search");
+			System.out.println(" 4 - Exit");
+
+			System.out.print("Please, enter your chosen action: ");
+			validVariant = (int) scannerValidate.getValidValue(TypeValidation.VARIANTS_4);
+
+			switch (validVariant) {
+			case (1):
+				enterData.menu();
+				break;
+			case (2):
+				sortData.menu();
+				break;
+			case (3):
+				searchData.menu();
+				break;
+			default:
+			}
+
+			if (validVariant == 4) {
+				System.out.println("Exiting...");
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.execute();
+		
+		SingletoneArray array = SingletoneArray.getInstance();
+		
+		for (int i = 0; i < array.size(); i ++)
+		{
+			System.out.println(array.get(i).toString());
+		}
+	}
 }
