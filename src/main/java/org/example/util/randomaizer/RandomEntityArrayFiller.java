ff6fd7bbcd5c7060eb276ca195d56f039dd1054d
Napwob <gorno-altaisk0@mail.ru>
2024-11-24 21:37:57 +0300

    Refactoring RandomFiller

--- src/main/java/org/example/util/randomaizer/RandomEntityArrayFiller.java
+++ src/main/java/org/example/util/randomaizer/RandomEntityArrayFiller.java
@@ -1,33 +1,25 @@
 package org.example.util.randomaizer;
 
 public class RandomEntityArrayFiller {
-    public static Object[] fillArray(String numberOfEssence, int arraySize) {
+    public static Object[] fillArray(String typeOfEntity, int arraySize) {
         Object[] array = new Object[arraySize];
-        switch (numberOfEssence) {
-            case ("1"):
+        switch (typeOfEntity) {
+	        case ("Animal"):
+	            for (int i = 0; i < arraySize; i++) {
+	                array[i] = RandomEntityGenerator.randomAnimalGenerate();
+	            }
+	            break;
+	        case ("Barrel"):
+	            for (int i = 0; i < arraySize; i++) {
+	                array[i] = RandomEntityGenerator.randomBarrelGenerate();
+	            }
+	            break;
+            case ("Human"):
                 for (int i = 0; i < arraySize; i++) {
                     array[i] = RandomEntityGenerator.randomHumanGenerate();
                 }
                 break;
-            case ("2"):
-                for (int i = 0; i < arraySize; i++) {
-                    array[i] = RandomEntityGenerator.randomAnimalGenerate();
-                }
-                break;
-            case ("3"):
-                for (int i = 0; i < arraySize; i++) {
-                    array[i] = RandomEntityGenerator.randomBarrelGenerate();
-                }
-                break;
-            case ("q"):
-                for (int i = 0; i < arraySize; i++) {
-                    System.exit(0);
-                }
-                break;
             default:
-                System.out.println("Нет указанного варианта! Попробуйте снова: ");
-                fillArray(numberOfEssence, arraySize);
-                break;
         }
         return array;
     }
