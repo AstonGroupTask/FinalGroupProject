a5309640fbd5ff38de707d74197ca6d948507d68
PhilippSergeevich <citruslima@yandex.ru>
2024-11-25 15:12:25 +0300

    Обновленная версия чтения из файла, адаптирован паттерн стратегии для заполнения из файла, внесены корректировки параметров

--- src/main/java/org/example/strategies/fill/FillContext.java
+++ src/main/java/org/example/strategies/fill/FillContext.java
@@ -1,16 +1,17 @@
 package org.example.strategies.fill;
 
-public class FillContext<T> {
-    private FillsStrategy<T> fillStrategy;
 
-    public void setFillStrategy(FillsStrategy<T> fillStrategy) {
+public class FillContext {
+    private FillsStrategy fillStrategy;
+
+    public void setFillStrategy(FillsStrategy fillStrategy) {
         this.fillStrategy = fillStrategy;
     }
 
-    public T[] fillArray(int size) {
+    public Comparable<?>[] fillArray(int size) {
         if (fillStrategy == null) {
             throw new IllegalStateException("Стратегия заполнения не установлена.");
         }
-        return fillStrategy.fill(size);
+        return fillStrategy.fill();
     }
 }
--- src/main/java/org/example/strategies/fill/FillsStrategy.java
+++ src/main/java/org/example/strategies/fill/FillsStrategy.java
@@ -1,7 +1,5 @@
 package org.example.strategies.fill;
 
-public interface FillsStrategy<T> {
-    T[] fill(int size);  // Метод для случайного и ручного ввода (без пути)
-
-    Object[] fillFromFile(int size, String filePath);
+public interface FillsStrategy {
+    Comparable<?>[] fill();
 }
