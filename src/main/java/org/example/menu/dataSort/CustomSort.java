45c6c245a6a2fec5f5739de1d0621f435beec8cf
Napwob <gorno-altaisk0@mail.ru>
2024-11-26 22:07:18 +0300

    Fix null element error 2

--- src/main/java/org/example/menu/dataSort/CustomSort.java
+++ src/main/java/org/example/menu/dataSort/CustomSort.java
@@ -2,18 +2,21 @@ package org.example.menu.dataSort;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class CustomSort implements SortingStrategy<Comparable> {
 
 	private String sortBy;
 
-	public void setSortBy(String SortBy) {
-		sortBy = SortBy;
+	public void setSortBy(String sortBy) {
+		this.sortBy = sortBy;
 	}
 
 	@Override
 	public void sort(Object[] array) {
-		Arrays.sort(array, (o1, o2) -> {
+		Object[] nonNullArray = Arrays.stream(array).filter(o -> o != null).toArray();
+
+		Arrays.sort(nonNullArray, (o1, o2) -> {
 			try {
 				Field field1 = o1.getClass().getDeclaredField(sortBy);
 				Field field2 = o2.getClass().getDeclaredField(sortBy);
@@ -23,10 +26,22 @@ public class CustomSort implements SortingStrategy<Comparable> {
 				Comparable value1 = (Comparable) field1.get(o1);
 				Comparable value2 = (Comparable) field2.get(o2);
 
+				if (value1 == null && value2 == null) {
+					return 0;
+				}
+				if (value1 == null) {
+					return -1;
+				}
+				if (value2 == null) {
+					return 1;
+				}
+
 				return value1.compareTo(value2);
 			} catch (NoSuchFieldException | IllegalAccessException e) {
 				throw new RuntimeException("Error accessing field: " + sortBy, e);
 			}
 		});
+
+		System.arraycopy(nonNullArray, 0, array, 0, nonNullArray.length);
 	}
 }
