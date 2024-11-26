64cb9fc80ccd6aca5ea034df39d1cbea5fe2058b
Napwob <gorno-altaisk0@mail.ru>
2024-11-26 22:05:15 +0300

    Add TODO message

--- src/main/java/org/example/menu/dataSort/QuickSort.java
+++ src/main/java/org/example/menu/dataSort/QuickSort.java
@@ -3,6 +3,6 @@ package org.example.menu.dataSort;
 public class QuickSort implements SortingStrategy<Comparable> {
 	@Override
 	public void sort(Object[] array) {
-		// TODO
+		System.out.print("<####[WORK_IN_PROGRESS]####>");
 	}
 }
